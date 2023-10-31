f9710acbb28c29b7094bccdfe6cff3339b655aa9
Danny Highley <highledl@mail.uc.edu>
2023-10-31 15:35:35 -0400

    Week 10

--- python/weatherapi.py
+++ python/weatherapi.py
@@ -4,7 +4,7 @@ import requests
 print('Please enter your zip code:')
 zip = input()
 
-r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=%s,us&appid=1543bf2e4a545f844b2e63d35589df9e' % zip)
+r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=%s,us&appid=API_KEY' % zip)
 data = r.json()
 
 print(data['weather'][0]['description'])

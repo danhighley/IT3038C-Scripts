d7f1140894cffcc65bd1716b29fbaca88d83eefc
Danny Highley <highledl@mail.uc.edu>
2023-11-01 11:20:23 -0400

    Lab 9

---
+++ python/lab9_widgets.py
@@ -0,0 +1,15 @@
+import json
+import requests
+
+# Pull in json data from node.js server running on same system.
+r = requests.get('http://localhost:3000/')
+data = r.json()
+
+print()  # formatting
+
+# Iterate through each element of the json data.
+for i in data:
+    print(i['name']+" is "+i['color'])
+
+print() # formatting
+    
